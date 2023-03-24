069fee95feb6afe751563d9a6eac19ada6412f67
Igor Oksentyuk <winbka.lem@gmail.com>
2023-03-24 13:17:36 +0200

    Imported genres module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { CarouselModule } from './carousel/carousel.module';
+import { GenresComponent } from './genres/genres.component';
+import { GenresModule } from './genres/genres.module';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, CarouselModule],
+  imports: [BrowserModule, AppRoutingModule, CarouselModule, GenresModule],
   providers: [],
   bootstrap: [AppComponent],
 })
